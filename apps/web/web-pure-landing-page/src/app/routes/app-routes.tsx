887dd7d62747f1985d6050ac4823c044f4a9b7d9
ErickEmpresa <erickcelestimo@gmail.com>
2024-10-09 11:59:48 -0300

    refactor: ajust deploy file 3

--- apps/web/web-pure-landing-page/src/app/routes/app-routes.tsx
+++ apps/web/web-pure-landing-page/src/app/routes/app-routes.tsx
@@ -4,9 +4,9 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 export const AppRouters = () => {
   return (
     <Routes>
-      <Route path="/home112" element={<HomeContainer />} />
+      <Route path="/home12" element={<HomeContainer />} />
 
-      <Route path="*" element={<Navigate to="/home112" />} />
+      <Route path="*" element={<Navigate to="/home12" />} />
     </Routes>
   );
   //
